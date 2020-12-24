5b1dc32c9b476bde5ff4dbb15ea86c0ff03619cc
Cam Dowless <camdowless@gmail.com>
2020-12-23 20:00:50 -0500

    Add files for deployment

--- app/core/settings.py
+++ app/core/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIFFILES_DIRS = [os.path.join()]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 """ASGI_APPLICATION = "core.routing.application"
 CHANNEL_LAYERS = {
